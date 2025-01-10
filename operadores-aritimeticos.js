b8f8637ff5d85dbc3c8bc4406c1905583b510092
Emílio Silva <emiliosarges@gmail.com>
2025-01-10 19:24:44 -0300

    Initial commit

---
+++ operadores-aritimeticos.js
@@ -0,0 +1,22 @@
+// const nota1 = 8;
+// const nota2 = 6.3;
+// const nota3 = 7;
+// const nota4 = 9.3;
+
+// let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// if (media >=7) {
+//     media+= media * 0.1;
+// }
+
+// console.log(`A média é ${media}`);
+
+const salarioMensal = 3500; 
+const despesasFixas = 1200; 
+const despesasVariaveis = 500; 
+const economiasMensais = 800;
+const bonusAnual = 3000;
+
+const resultado = (salarioMensal - despesasFixas - despesasVariaveis) * 12 + (economiasMensais * 12) + bonusAnual;
+
+console.log(resultado);
---
+++ teste.js
@@ -0,0 +1,50 @@
+// Programação é prática! Criamos mais uma lista de atividades (não obrigatórias) para você exercitar e reforçar ainda mais seu aprendizado. Bora praticar?
+
+// Crie uma variável contendo uma frase. Em seguida, exiba no console o comprimento da frase e a mesma frase com letras maiúsculas.
+
+// Declare duas variáveis, uma inicializada com valor null e outra que não recebe atribuição (resultando em undefined). Exiba os valores no console.
+
+// Crie 3 variáveis de tipos diferentes, utilize template strings para combinar os diferentes tipos de dados (number, string, boolean) em uma única string e exiba o resultado no console.
+
+// Crie uma variável numérica e uma string. Faça a conversão da variável numérica para string e da string para número. Exiba os tipos de dados resultantes no console.
+
+// Crie uma variável com uma string e utilize métodos de manipulação de strings, como toUpperCase, toLowerCase, slice ou outros, para modificar a string original. Exiba os resultados finais no console.
+
+
+// let frase = "Olá, mundo!";
+// console.log(frase.toLowerCase());
+//-----------------------------------------
+// let primeira = null;
+// let segunda;
+// console.log(primeira);
+// console.log(segunda);
+//-----------------------------------------
+// let primeira = 5;
+// let segunda = "Texto";
+// let terceira = true;
+// const total = `Você tem um ${segunda} de ${primeira} letras! ${terceira}`;
+// console.log(total);
+//-----------------------------------------
+// let num = 5;
+// let str = "10";
+
+// console.log(num);
+// console.log(str);
+
+// console.log(typeof num);
+// console.log(typeof str);
+
+// const numstr = Number(str);
+// const strnum = num.toString();
+
+// console.log(numstr);
+// console.log(strnum);
+
+// console.log(typeof numstr);
+// console.log(typeof strnum);
+
+// const frase = "Testandos Strings - beM testadaS";
+// console.log(frase.toLocaleLowerCase());
+// console.log(frase.toUpperCase());
+// console.log(frase.slice(0,17));
+
